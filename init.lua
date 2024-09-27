120c032d74a306b07c29522ff262bda6a65fceb7
David Oyinbo <dev.davexoyinbo@gmail.com>
2024-09-28 00:53:07 +0100

    .

--- init.lua
+++ init.lua
@@ -1,2 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+require("luasnip").setup({
+  history = true,
+  region_check_events = "InsertEnter",
+  delete_check_events = "TextChanged,InsertLeave",
+})
